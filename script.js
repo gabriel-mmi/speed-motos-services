9cef9affec079f7c6e2606deef9a0119482bb7df
Ckyre <ckyre.game@gmail.com>
2020-10-20 15:16:42 +0200

    Ajout de la liste d'articles a la boutique

--- script.js
+++ script.js
@@ -7,12 +7,32 @@ function OnNextImage (step = 1) {
         circlesParent.children[index].classList.remove("active");       
     }
 
-    if(currentImage < imagesParent.children.length - 1) {
-        currentImage += step;
-    }else {
-        currentImage = 0;
+    if(step < 0) {
+        if(currentImage > 0) {
+            currentImage += step;
+        }else {
+            currentImage = imagesParent.children.length - 1;
+        }
+    }else if (step > 0) {
+        if(currentImage < imagesParent.children.length - 1) {
+            currentImage += step;
+        }else {
+            currentImage = 0;
+        }
     }
     
     imagesParent.children[currentImage].classList.add("active");
     circlesParent.children[currentImage].classList.add("active");
 }
+
+function OnSelectShopCategory (itemName) {
+    document.getElementById("galerie").style.display = "none";
+    document.getElementById("articles-list").style.display = "block";
+
+    document.getElementById("casques").style.display = "none";
+    document.getElementById("gans").style.display = "none";
+    document.getElementById("vestes").style.display = "none";
+
+    document.getElementById(itemName).style.display = "block";
+}
+
