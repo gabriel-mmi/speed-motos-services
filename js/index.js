5b21253be7c2df3a3ac4e855448e17749ff3966a
Ckyre <ckyre.game@gmail.com>
2020-10-22 18:06:38 +0200

    Ajout de la 3D

--- js/index.js
+++ js/index.js
@@ -70,8 +70,6 @@ const left= document.querySelector(".text_left");
 const right= document.querySelector(".text_right");
 const img= document.querySelector(".overlay_container_moto img");
 
-console.log(right);
-
 overlay.addEventListener("mouseover", function(){
     left.style.left = "40%";
     right.style.right= "40%";
@@ -81,3 +79,4 @@ overlay.addEventListener("mouseout", function(){
     left.style.left = "50%";
     right.style.right= "50%";
 });
+
