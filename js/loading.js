6ef0ba6807d22e711b815bbf35c89d53b942033b
Ckyre <ckyre.game@gmail.com>
2020-10-23 11:35:56 +0200

    Animations de scroll

--- js/loading.js
+++ js/loading.js
@@ -1,10 +1,10 @@
 document.body.style.overflowY = "hidden";
-console.log("loading");
+
 window.addEventListener("load", function () {
     setTimeout(() => {
         document.getElementById("loading").style.opacity = "0";
         document.body.style.overflowY = "scroll";
-        console.log("loaded");
+
         setTimeout(() => {
             document.getElementById("loading").style.display = "none";
         }, 500);
